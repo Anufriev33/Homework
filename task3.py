a8154c1e1726eca1f82edd94f970348348921ec0
roman <roma.anufriev.90@mail.ru>
2023-07-13 19:25:28 +0300

    decision3

---
+++ task3.py
@@ -0,0 +1,14 @@
+#Задача №3
+
+#Требуется вывести все целые степени двойки (т.е. числа вида 2k), не превосходящие числа N.
+
+#Решение :
+
+n = int(input("Введите число:"))
+for i in range(n):
+    power_of_two = 2 ** i
+    if power_of_two <= n:
+        print(power_of_two)
+    else:
+        break
+
