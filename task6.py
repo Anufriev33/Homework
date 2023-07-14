002ed66ad778519fca9831e6d4767794b79e732d
roman <roma.anufriev.90@mail.ru>
2023-07-14 20:33:35 +0300

    task6

--- task6.py
+++ task6.py
@@ -90,3 +90,24 @@ else:
 
 # Выводим стоимость слова
 print("Стоимость введенного слова:", score)
+
+
+word = k.upper()
+summ= 0
+for i in word:
+    for k, v in list_letters.items()
+
+    points_en = {1: 'AEIOULNSTR', 2: 'DG', 3: 'BCMP', 4: 'FHVWY', 5: 'K', 8: 'JX', 10: 'QZ'}
+points_ru = {1: 'АВЕИНОРСТ', 2: 'ДКЛМПУ', 3: 'БГЁЬЯ', 4: 'ЙЫ', 5: 'ЖЗХЦЧ', 8: 'ШЭЮ', 10: 'ФЩЪ'}
+word = k.upper()  # переводим все буквы в верхний регистр
+count = 0
+for i in word:
+    if i in 'QWERTYUIOPASDFGHJKLZXCVBNM':
+        for j in points_en:
+            if i in points_en[j]:
+                count = count + j
+    else:
+        for j in points_en:
+            if i in points_ru[j]:
+                count = count + j
+print(count)
