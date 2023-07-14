bf7bd601f00451e3e12f4d6fcfdf0e72614595eb
roman <roma.anufriev.90@mail.ru>
2023-07-14 20:20:43 +0300

    task5

--- task5.py
+++ task5.py
@@ -3,24 +3,26 @@
 # Требуется найти в массиве list_1 
 # самый близкий по величине элемент к заданному числу k и вывести его.
 
-list_1 = [1, 2, 3, 4, 5]
-k = 6
+# list_1 = [1, 2, 3, 4, 5]
+# k = 6
 
-closest_element = list_1[0]  # предполагаем, что первый элемент ближайший
-min_diff = abs(k - closest_element)  # предполагаем, что разница с первым элементом наименьшая
+# closest_element = list_1[0]  # предполагаем, что первый элемент ближайший
+# min_diff = abs(k - closest_element)  # предполагаем, что разница с первым элементом наименьшая
 
-for num in list_1:
-    diff = abs(k - num)  # находим разницу между числом k и текущим элементом
-    if diff < min_diff:  # если разница меньше минимальной
-        min_diff = diff  # обновляем минимальную разницу
-        closest_element = num  # обновляем ближайший элемент
+# for num in list_1:
+#     diff = abs(k - num)  # находим разницу между числом k и текущим элементом
+#     if diff < min_diff:  # если разница меньше минимальной
+#         min_diff = diff  # обновляем минимальную разницу
+#         closest_element = num  # обновляем ближайший элемент
 
-print(closest_element)  # выводим ближайший элемент
+# print(closest_element)  # выводим ближайший элемент
+
+
                
-m = abs(k - list_1[0])  # модуль числа
-number = list_1[0]
-for i in range(1, len(list_1)):
-    if m > abs(list_1[i] - k):
-        m = abs(list_1[i] - k)
-        number = list_1[i]
-print(number)
+# m = abs(k - list_1[0])  # модуль числа
+# number = list_1[0]
+# for i in range(1, len(list_1)):
+#     if m > abs(list_1[i] - k):
+#         m = abs(list_1[i] - k)
+#         number = list_1[i]
+# print(number)
